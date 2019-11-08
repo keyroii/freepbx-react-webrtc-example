27ef14106b9c6c27adc2bab8920a32bcc660a4d8
Kevin Müller <Kevin.Mueller@tel-inform.de>
2019-11-08 15:10:56 +0100

    Updated to newest version, added EventHandler Examples

--- src/App.js
+++ src/App.js
@@ -3,7 +3,60 @@ import './App.css';
 
 import WebRTCClient from "freepbx-react-webrtc";
 
+import EventEmitter from "events";
+import Button from 'react-bootstrap/Button';
+
 function App() {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+        eventHandler: new EventEmitter(),
+        currentCallState: null,
+        currentConnectionState: null
+    };
+  }
+
+  componentDidMount() {
+    // Connected and Idle
+    this.state.eventHandler.on("Idle", function() {
+      // Do something...
+    });
+
+    // Outgoing call happening
+    this.state.eventHandler.on("Calling", function() {
+      // Do something...
+    });
+
+    // Incoming Call
+    this.state.eventHandler.on("Alerting", function() {
+      // Do something...
+    });
+
+    // Call started
+    this.state.eventHandler.on("InCall", function() {
+      // Do something...
+    });
+  };
+
+  eventHandlerEmit = (emit) => {
+    this.state.eventHandler.emit(emit);
+  }
+
+  updateCallState = (callState) => {
+    this.setState({
+      ...this.state,
+      currentCallState: callState
+    })
+  }
+
+  updateConnectionState = (connectionState) => {
+    this.setState({
+      ...this.state,
+      currentConnectionState: connectionState
+    })
+  }
+
   return (
     <div className="App">
       <header>
@@ -12,6 +65,11 @@ function App() {
       <body>
         <video width="25%" height="25%" id="localVideo" autoPlay playsInline  muted="muted"></video>
         <video width="50%" height="50%" id="remoteVideo" autoPlay playsInline ></video>
+
+        <Button onClick={() => this.eventHandlerEmit("answerCall")}>Accept Incoming Call</Button>
+        <Button onClick={() => this.eventHandlerEmit("hangupCall")}>Hangup Call</Button>
+        <Button onClick={() => this.eventHandlerEmit("placeCall")}>Start Call</Button>
+
         <WebRTCClient
             enableVideo={true}
             enableSound={true}
@@ -28,6 +86,11 @@ function App() {
             localVideoTagId={"localVideo"} // ID of video tag for own video
             remoteVideoTagId={"remoteVideo"} // ID of video tag for incoming remote video
             skipStunServer={true} // True if Server is only reachable in local network and is not public
+            eventHandler={this.state.eventHandler}
+            eventHandlerEmit={this.eventHandlerEmit}
+            updateCallState={this.updateCallState}
+            updateConnectionState={this.updateConnectionState}
+            traceSip={true}
         />
       </body>
     </div>
